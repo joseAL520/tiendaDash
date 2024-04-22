2a7e7012ddca41d7ba1187ebad0ac21474fd3e25
jose angel <jose.albelt1817@gmail.com>
2024-04-22 12:05:20 -0500

    login, style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./componets/login/login.component";
-import { RegisterComponent } from "./componets/register/register.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, LoginComponent, RegisterComponent]
+    imports: [RouterOutlet, LoginComponent]
 })
 export class AppComponent {
   title = 'ProyectInventari';
--- src/app/componets/login/login.component.ts
+++ src/app/componets/login/login.component.ts
@@ -1,12 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  public formLogin: FormGroup
+
+  constructor (
+      private formBuilder: FormBuilder
+  ){  
+    this.formLogin = formBuilder.group({
+        user:     ['',[Validators.required]],
+        password: ['',[Validators.required]]
+    })
+  }
+
+  validator(controlName:string, typeError:string){
+    return this.formLogin.get(controlName)?.hasError(typeError) && this.formLogin.get(controlName)?.touched;
+  }
+
+  login(){
+    if(this.formLogin.status === 'INVALID'){
+      alert('FAVOR DE LLENAR TODO LOS CAMPOS')
+    }else{
+      console.log('goo');
+    }
+
+  }
 
 }
