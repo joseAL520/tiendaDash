8992a75fa26e9b60a9ce5b7954d1242c7b64ab63
jose angel <jose.albelt1817@gmail.com>
2024-04-22 09:01:43 -0500

    structure proyect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./componets/login/login.component";
+import { RegisterComponent } from "./componets/register/register.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, LoginComponent, RegisterComponent]
 })
 export class AppComponent {
   title = 'ProyectInventari';
---
+++ src/app/componets/dashboar/dashboar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboar',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboar.component.html',
+  styleUrl: './dashboar.component.css'
+})
+export class DashboarComponent {
+
+}
