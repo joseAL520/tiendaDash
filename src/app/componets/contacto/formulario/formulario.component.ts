80e9692b6b3d788fa8384bf8f3f78474766afd30
jose angel <jose.albelt1817@gmail.com>
2024-04-25 08:52:18 -0500

    form and tablet of contact

--- src/app/componets/contacto/formulario/formulario.component.ts
+++ src/app/componets/contacto/formulario/formulario.component.ts
@@ -1,12 +1,49 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
 export class FormularioContactComponent {
+
+
+  formularioContacto: FormGroup 
+
+  constructor(
+      private form: FormBuilder,
+      // private _servicioContacto: ServicioContactoService
+  ){
+    this.formularioContacto = this.form.group({
+        numeroIdentfiacion: ['',[Validators.required, Validators.minLength(9)]],
+        nombre: ['',[Validators.required]],
+        apellido: ['',[Validators.required]],
+        correo: ['',[Validators.required]],
+        numero: ['',[Validators.required, Validators.minLength(10)]],
+        direccion: ['',[Validators.required]],
+        describcion: [''],
+    })
+  }
+
+
+  validator(controlName: string , tipoError:string){
+    return this.formularioContacto.get(controlName)?.hasError(tipoError) && this.formularioContacto.get(controlName)?.touched
+  }
+
+  addProduct(){
+
+    if (this.formularioContacto.valid) {
+      const contactoData = this.formularioContacto.value;
+      // this._servicioContacto.registrarContacto(contactoData).subscribe()
+      this.formularioContacto.reset()
+    } else {
+      alert('favo de llenar los campos');
+    }
+    }
+
 
 }
--- src/app/componets/contacto/tabla/tabla.component.ts
+++ src/app/componets/contacto/tabla/tabla.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tabla',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tabla.component.html',
   styleUrl: './tabla.component.css'
 })
 export class TablaContactComponent {
+
+  public listaContacto: any [] = []
+
+ 
+  constructor(
+      //  private _apiMock: ServicioContactoService
+  ){}
+
+
+  ngOnInit():void{
+      // this._apiMock.getListContact().subscribe(data => {
+      //   this.listaContacto = Object.values(data);
+      //   console.log(data);
+      // })
+  }
+
 
 }
